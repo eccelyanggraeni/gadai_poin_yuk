f11a75e0dcb376746fe95c371e84f86ba0728536
Gabe Dimas W <gabe.dimas10@gmail.com>
2020-01-10 10:24:29 +0700

    Update for login.php, register.php, users.sql
    Add "Invalid username or password" string to upper text field
    
    Add  "Alamat, No.Hp, and CIF Number" Text field
    
    Update users.sql according from the register.php brand new text field

--- sql/users.sql
+++ sql/users.sql
@@ -1,5 +1,9 @@
 CREATE TABLE `user` (
 `id` int(11) NOT NULL,
+`name` varchar(255) NOT NULL,
+-- `alamat` varchar(255) NOT NULL,
+-- `nohp` varchar(255) NOT NULL,
+-- `cif` varchar(255) NOT NULL,
 `username` varchar(100) NOT NULL,
 `email` varchar(100) NOT NULL,
 `password` varchar(100) NOT NULL
